29da61311b5acf8e9ca665f175e23bcfd7710f7b
Greatshunge <shunone@139.com>
2018-09-30 17:39:34 +0800

    Signed-off-by: Greatshunge <shunone@139.com>

--- src/main.js
+++ src/main.js
@@ -6,8 +6,12 @@ import router from './router'
 import 'styles/reset.css'
 import 'styles/border.css'
 import 'styles/iconfont.css'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 
+// require styles
+import 'swiper/dist/css/swiper.css'
 Vue.config.productionTip = false
+Vue.use(VueAwesomeSwiper)
 
 /* eslint-disable no-new */
 new Vue({
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,47 @@
+<template>
+	<div class="wrapper">
+		<swiper :options="swiperOption">
+	    <!-- slides -->
+	    <swiper-slide v-for='item of swiperList' :key='item.id'>
+	    	<img class="swiper-img" :src="item.imgUrl" />
+	    </swiper-slide>
+	    <!-- Optional controls -->
+	    <div class="swiper-pagination"  slot="pagination"></div>
+	  </swiper>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'HomeSwiper',
+		data (){
+			return {
+				swiperOption: {
+					pagination:'.swiper-pagination',
+					loop:true
+				},
+				swiperList: [{
+					id:'0001',
+					imgUrl:'http://img1.qunarzz.com/piao/fusion/1809/6a/847ac7b98f10e202.jpg_750x200_f0593cb5.jpg'
+					},{
+						id:'0002',
+						imgUrl:'http://img1.qunarzz.com/piao/fusion/1809/61/44400d6591891202.jpg_750x200_7565a05c.jpg'
+				}]
+			}
+			
+		}
+	}
+</script>
+
+<style lang="stylus" scoped>
+	.wrapper >>> .swiper-pagination-bullet-active
+	  background: #fff
+	.wrapper
+	  overflow:hidden
+	  width: 100%
+	  height:0
+	  padding-bottom: 26.66%
+	  background: #eee
+	  .swiper-img
+	    width: 100%
+</style>
